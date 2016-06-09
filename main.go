718a273412d83d6f436007a8cecc314e9ab58bc0
Demitrious Kelly <demitrious@automattic.com>
2016-06-08 21:25:33 -0700

    in development

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "flag"
+import (
+	"github.com/FederationOfFathers/dashboard-api"
+	"github.com/FederationOfFathers/dashboard-slack"
+	"github.com/apokalyptik/cfg"
+	"github.com/uber-go/zap"
+)
+
+var slackApiKey = "xox...."
+var logger = zap.NewJSON()
 
 func init() {
+	scfg := cfg.New("slack")
+	scfg.StringVar(&slackApiKey, "apiKey", slackApiKey, "Slack API Key (env: SLACK_APIKEY)")
 }
 
 func main() {
-	flag.Parse()
+	cfg.Parse()
+	logger.SetLevel(zap.Info)
+	data, err := bot.SlackConnect(slackApiKey)
+	if err != nil {
+		logger.Fatal("Unable to contact the slack API", zap.Err(err))
+	}
+	api.Run(data)
 }
