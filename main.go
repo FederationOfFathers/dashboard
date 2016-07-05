9145af8f41f545856fd76299ccd1300172d9dabe
apokalyptik <apokalyptik@apokalyptik.com>
2016-07-05 16:47:45 +0000

    configuration updates

--- main.go
+++ main.go
@@ -8,17 +8,19 @@ import (
 	"github.com/uber-go/zap"
 )
 
-var slackApiKey = "xox...."
+var slackAPIKey = "xox...."
 var logger = zap.NewJSON()
 
 func init() {
 	scfg := cfg.New("cfg-slack")
-	scfg.StringVar(&slackApiKey, "apiKey", slackApiKey, "Slack API Key (env: SLACK_APIKEY)")
+	scfg.StringVar(&slackAPIKey, "apiKey", slackAPIKey, "Slack API Key (env: SLACK_APIKEY)")
 	scfg.StringVar(&bot.CdnPrefix, "cdnPrefix", bot.CdnPrefix, "http url base from which to store saved uploads")
 	scfg.StringVar(&bot.CdnPath, "cdnPath", bot.CdnPath, "Filesystem path to store uploads in")
 
 	acfg := cfg.New("cfg-api")
 	acfg.StringVar(&api.ListenOn, "listen", api.ListenOn, "API bind address (env: API_LISTEN)")
+	acfg.StringVar(&api.AuthSecret, "secret", api.AuthSecret, "Authentication secret for use in generating login tokens")
+	acfg.StringVar(&api.JWTSecret, "hmac", api.JWTSecret, "Authentication secret used for JWT tokens")
 
 	ecfg := cfg.New("cfg-events")
 	ecfg.StringVar(&events.SaveFile, "savefile", events.SaveFile, "path to the file in which events should be persisted")
@@ -28,7 +30,7 @@ func init() {
 
 func main() {
 	cfg.Parse()
-	data, err := bot.SlackConnect(slackApiKey)
+	data, err := bot.SlackConnect(slackAPIKey)
 	events.Start(data)
 	if err != nil {
 		logger.Fatal("Unable to contact the slack API", zap.Error(err))
