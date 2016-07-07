cb9db492d204c8803ed4f9ceb6a64e630cb762a4
apokalyptik <apokalyptik@apokalyptik.com>
2016-07-07 18:25:40 +0000

    serve static assets from the dashboard-ui repo

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
+//go:generate echo "---[ making sure fileb0x is installed ]"
+//go:generate go get -v github.com/UnnoTed/fileb0x
+//go:generate echo "---[ updating ../dashboard-ui ]"
+//go:generate /bin/bash -c "cd ../dashboard-ui && git pull"
+//go:generate echo "---[ importing ../dashboard-ui/application/ ]"
+//go:generate fileb0x ./b0x.json
+//go:generate echo "---[ building ]"
+//go:generate go build -v
+//go:generate echo "---[ done ]"
 package main
 
 import (
+	"net/http"
+
 	"github.com/FederationOfFathers/dashboard/api"
 	"github.com/FederationOfFathers/dashboard/events"
 	"github.com/FederationOfFathers/dashboard/slack"
+	"github.com/FederationOfFathers/dashboard/ui"
 	"github.com/apokalyptik/cfg"
 	"github.com/uber-go/zap"
 )
@@ -25,7 +37,6 @@ func init() {
 	ecfg := cfg.New("cfg-events")
 	ecfg.StringVar(&events.SaveFile, "savefile", events.SaveFile, "path to the file in which events should be persisted")
 	ecfg.DurationVar(&events.SaveInterval, "saveinterval", events.SaveInterval, "how often to check and see if we need to save data")
-
 }
 
 func main() {
@@ -35,5 +46,6 @@ func main() {
 	if err != nil {
 		logger.Fatal("Unable to contact the slack API", zap.Error(err))
 	}
+	api.Router.PathPrefix("/application/").Handler(http.FileServer(ui.HTTP))
 	api.Run(data, events.Data)
 }
