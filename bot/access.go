ce8884cdb9a61d342100c2333d517cd7901120b5
apokalyptik <apokalyptik@apokalyptik.com>
2016-09-04 18:18:23 +0000

    hardcode some admins

--- bot/access.go
+++ bot/access.go
@@ -3,6 +3,14 @@ package bot
 var AdminChannel = "damnbot-admin"
 
 func IsUserIDAdmin(userID string) (bool, error) {
+	switch userID {
+	case "U03RTQMPB": // demitriousk
+		return true, nil
+	case "U0381RKM9": // admwrightmeow
+		return true, nil
+	case "U0920GS5S": // bobthelob
+		return true, nil
+	}
 	for _, channel := range data.UserGroups(userID) {
 		if channel.Name == AdminChannel {
 			return true, nil
