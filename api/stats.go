8c64204f0fd47596b1507664342d87f29cb45c8c
apokalyptik <apokalyptik@apokalyptik.com>
2016-11-28 15:54:56 +0000

    work around the inflexible fetch api that the ui uses in the backend because: fn javascript hype...

--- api/stats.go
+++ api/stats.go
@@ -6,19 +6,28 @@ import (
 	"net/http"
 )
 
+func statsPassthrough(uri string, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	rsp, err := http.Get(fmt.Sprintf("http://127.0.0.1:8874%s", uri))
+	if rsp != nil {
+		defer rsp.Body.Close()
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	io.Copy(w, rsp.Body)
+}
+
 func init() {
+	Router.PathPrefix("/api/v0/xhr/stats/").Methods("GET").Handler(jwtHandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			statsPassthrough(r.URL.Path[17:], w, r)
+		},
+	))
 	Router.PathPrefix("/xhr/stats/").Methods("GET").Handler(jwtHandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			rsp, err := http.Get(fmt.Sprintf("http://127.0.0.1:8874%s", r.URL.Path[10:]))
-			if rsp != nil {
-				defer rsp.Body.Close()
-			}
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			io.Copy(w, rsp.Body)
+			statsPassthrough(r.URL.Path[10:], w, r)
 		},
 	))
 }
