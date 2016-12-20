a56f87d579ffae4d7ac1f7db9056f23959937b1f
apokalyptik <apokalyptik@apokalyptik.com>
2016-12-20 16:50:32 +0000

    send dev login from fofbot

--- bot/login.go
+++ bot/login.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/nlopes/slack"
 )
@@ -15,6 +14,17 @@ type AuthTokenGeneratorFunc func(string) []string
 
 var AuthTokenGenerator = func(s string) []string {
 	return nil
+}
+
+func SendDevLogin(user string) {
+	var linkText = "Dev login with this link"
+	var msg = fmt.Sprintf(
+		"<%sapi/v0/login?w=%s&t=%s|%s>",
+		LoginLink,
+		user,
+		AuthTokenGenerator(user)[0],
+		linkText)
+	fofbotMessage <- sendMessage{user, msg}
 }
 
 func handleDevLogin(m *slack.MessageEvent) bool {
@@ -33,32 +43,19 @@ func handleDevLogin(m *slack.MessageEvent) bool {
 		return false
 	}
 
-	linkText := "Dev login with this link"
+	SendDevLogin(m.Msg.User)
+	return true
+}
 
+func SendLogin(user string) {
+	var linkText = "Login with this link"
 	var msg = fmt.Sprintf(
 		"<%sapi/v0/login?w=%s&t=%s|%s>",
 		LoginLink,
-		m.Msg.User,
-		AuthTokenGenerator(m.Msg.User)[0],
+		user,
+		AuthTokenGenerator(user)[0],
 		linkText)
-
-	for i := 0; i < 5; i++ {
-		_, _, err := rtm.PostMessage(
-			m.Channel,
-			msg,
-			slack.PostMessageParameters{
-				Text:        msg,
-				AsUser:      true,
-				UnfurlLinks: true,
-				UnfurlMedia: true,
-				IconEmoji:   ":link:",
-			})
-		if err == nil {
-			break
-		}
-		time.Sleep(time.Duration(i) * time.Second)
-	}
-	return true
+	fofbotMessage <- sendMessage{user, msg}
 }
 
 func handleLogin(m *slack.MessageEvent) bool {
@@ -75,14 +72,6 @@ func handleLogin(m *slack.MessageEvent) bool {
 	if AuthTokenGenerator == nil {
 		return false
 	}
-
-	var linkText = "Login with this link"
-	var msg = fmt.Sprintf(
-		"<%sapi/v0/login?w=%s&t=%s|%s>",
-		LoginLink,
-		m.Msg.User,
-		AuthTokenGenerator(m.Msg.User)[0],
-		linkText)
-	fofbotMessage<- sendMessage{m.User,msg}
+	SendLogin(m.Msg.User)
 	return true
 }
