2d765e0f253f1112d9bf601cd195c74d22d5fbee
apokalyptik <apokalyptik@apokalyptik.com>
2016-12-21 17:07:41 +0000

    use X-Real-IP when available for logging IP address

--- api/logger.go
+++ api/logger.go
@@ -24,11 +24,15 @@ func (h *httpLogger) ServeHTTP(w http.ResponseWriter, r *http.Request, next http
 	start := time.Now()
 	var nw = &responseWriter{status: 200, ResponseWriter: w}
 	next(nw, r)
+	remoteAddress := r.RemoteAddr
+	if addr := r.Header.Get("X-Real-IP"); addr != "" {
+		remoteAddress = addr
+	}
 	logger.Info(
 		"HTTP Request",
 		zap.String("uri", r.RequestURI),
 		zap.Int("http_status", nw.status),
-		zap.String("remote_address", r.RemoteAddr),
+		zap.String("remote_address", remoteAddress),
 		zap.String("method", r.Method),
 		zap.Int64("content_length", r.ContentLength),
 		zap.Float64("response_time", time.Now().Sub(start).Seconds()))
