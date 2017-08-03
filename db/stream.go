e669523f92519bade58e0a21b174854043724d08
Bear Cherian <bearcherian@gmail.com>
2017-08-03 16:38:49 -0400

    Updating Mixer stream updating * Replaced beam.pro URLs and api calls with mixer.com URLs * Updating message if game changes * Updating messaging to include a structured attatchment

--- db/stream.go
+++ db/stream.go
@@ -14,6 +14,7 @@ type Stream struct {
 	YoutubeStart   int64     `gorm:"type:bigint;not null;default:0"`
 	YoutubeStop    int64     `gorm:"type:bigint;not null;default:0"`
 	Beam           string    `gorm:"type:varchar(191);not null;default:'';index"`
+	BeamGame       string    `form:"type:varchar(191);not null;default:''"`
 	BeamStart      int64     `gorm:"type:bigint;not null;default:0"`
 	BeamStop       int64     `gorm:"type:bigint;not null;default:0"`
 	CreatedAt      time.Time `sql:"DEFAULT:current_timestamp"`
