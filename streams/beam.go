bc4947e25a6b3549031e29111d4ff889de0e55e9
Bear Cherian <bearcherian@gmail.com>
2017-08-03 22:43:36 -0400

    Temp beam disable

--- streams/beam.go
+++ streams/beam.go
@@ -13,6 +13,7 @@ import (
 )
 
 var bplog = zap.New(zap.NewJSONEncoder()).With(zap.String("module", "streams"), zap.String("service", "beam"))
+var beamEnabled bool = false
 
 type beamChannelResponse struct {
 	Name  string `json:"name"`
@@ -142,6 +143,9 @@ func (b *Beam) startMessage(memberID int) (string, slack.PostMessageParameters, 
 }
 
 func mindBeam() {
+	if !beamEnabled {
+		return
+	}
 	bplog.Debug("begin minding")
 	for _, stream := range Streams {
 		if stream.Beam == "" {
