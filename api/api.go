d740ad5fd7e697d1e3c2246d148d8b622d6dfed0
apokalyptik <apokalyptik@apokalyptik.com>
2017-10-16 02:22:37 +0000

    update a bunch of handlers

--- api/api.go
+++ api/api.go
@@ -3,8 +3,10 @@ package api
 import (
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/FederationOfFathers/dashboard/db"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/uber-go/zap"
 )
@@ -17,6 +19,8 @@ var URLHostName = ""
 var URLScheme = "https"
 var DB *db.DB
 var UseHttps = false
+
+var allowedOrigins = regexp.MustCompile(`^https?://((localhost|127\.0\.0\.1)(:[0-9]+)?|([^.]*\.)?fofgaming.com)$`)
 
 func InitURLScheme() {
 	if UseHttps {
@@ -32,5 +36,28 @@ func Run() {
 	logger.Fatal(
 		"error starting API http server",
 		zap.String("listenOn", ListenOn),
-		zap.Error(http.ListenAndServe(ListenOn, Router)))
+		zap.Error(http.ListenAndServe(ListenOn,
+			handlers.ProxyHeaders(
+				handlers.CompressHandler(
+					handlers.CombinedLoggingHandler(os.Stdout,
+						handlers.CORS(
+							handlers.AllowCredentials(),
+							handlers.AllowedHeaders([]string{
+								"Content-Type",
+								"Authorization",
+								"X-Requested-With",
+							}),
+							handlers.AllowedMethods([]string{
+								"GET",
+								"HEAD",
+								"PUT",
+								"POST",
+								"DELETE",
+							}),
+							handlers.AllowedOriginValidator(allowedOrigins.MatchString),
+						)(Router),
+					),
+				),
+			),
+		)))
 }
