b27f1abbd7ec3faf3d984aeb82bcd4858b21ab14
apokalyptik <apokalyptik@apokalyptik.com>
2017-10-16 14:58:08 +0000

    always assign db

--- db/member.go
+++ db/member.go
@@ -21,7 +21,8 @@ type Member struct {
 }
 
 func (m *Member) Save() error {
-	return m.db.Save(m).Error
+	rval := m.db.Save(m)
+	return rval.Error
 }
 
 func (d *DB) MemberByAny(some string) (*Member, error) {
@@ -41,36 +42,28 @@ func (d *DB) MemberByAny(some string) (*Member, error) {
 func (d *DB) MemberByID(id int) (*Member, error) {
 	m := new(Member)
 	err := d.First(&m, id).Error
-	if err != nil {
-		m.db = d
-	}
+	m.db = d
 	return m, err
 }
 
 func (d *DB) MemberByName(name string) (*Member, error) {
 	m := new(Member)
 	err := d.DB.Where("name = ?", name).First(&m).Error
-	if err != nil {
-		m.db = d
-	}
+	m.db = d
 	return m, err
 }
 
 func (d *DB) MemberBySlackID(id string) (*Member, error) {
 	m := new(Member)
 	err := d.DB.Where("slack = ?", id).First(&m).Error
-	if err != nil {
-		m.db = d
-	}
+	m.db = d
 	return m, err
 }
 
 func (d *DB) MembersActive(since time.Time) ([]*Member, error) {
 	m := []*Member{}
 	err := d.DB.Where("seen >= ?", since.Unix()).Find(&m).Error
-	for _, i := range m {
-		i.db = d
-	}
+	i.db = d
 	return m, err
 }
 
