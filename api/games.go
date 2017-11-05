2913401d36da3f93fb02b5d8f6ca1d749d84c6ab
apokalyptik <apokalyptik@apokalyptik.com>
2017-11-05 22:13:58 +0000

    continue working on games api responses...

--- api/games.go
+++ api/games.go
@@ -9,7 +9,63 @@ import (
 	"go.uber.org/zap"
 )
 
+type gamePlatform int
+
+func (g gamePlatform) name() string {
+	if s, ok := map[int]string{
+		1: "Xbox360",
+		2: "XboxOne",
+		3: "PC",
+		4: "iOS",
+		5: "Android",
+		6: "Mobile",
+		7: "GearVR",
+		8: "Kindle",
+	}[int(g)]; ok {
+		return s
+	}
+	return "Unknown"
+}
+
+func (g gamePlatform) MarshalJSON() ([]byte, error) {
+	return []byte(g.name()), nil
+}
+
+func getPicforGameName(name string) string {
+	var rval string
+	var p int
+	rows, err := DB.Raw("SELECT platform,image FROM games WHERE name=?", name).Rows()
+	defer rows.Close()
+	if err != nil {
+		return rval
+	}
+	for rows.Next() {
+		var platform int
+		var image string
+		if err := rows.Scan(&platform, &image); err != nil {
+			continue
+		}
+		if image == "" {
+			continue
+		}
+		if platform == 2 {
+			return image
+		}
+		if p == 0 || p > platform {
+			p = platform
+			rval = image
+		}
+	}
+	return rval
+}
+
 func init() {
+	Router.Path("/api/v0/games/player/{id}/{days}.json").Methods("GET").Handler(jwtHandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+		}))
+	Router.Path("/api/v0/games/played/{game}/{days}.json").Methods("GET").Handler(jwtHandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+		}))
 	Router.Path("/api/v0/games/played/top/{days}/{number}.json").Methods("GET").Handler(jwtHandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			n, _ := strconv.Atoi(mux.Vars(r)["number"])
@@ -30,18 +86,23 @@ func init() {
 			w.Header().Set("Content-Type", "application/json")
 			var rval = []struct {
 				Name    string `json:"name"`
+				Image   string `json:"image"`
 				Players int    `json:"players"`
 			}{}
 			defer rows.Close()
 			for rows.Next() {
 				var row struct {
 					Name    string `json:"name"`
+					Image   string `json:"image"`
 					Players int    `json:"players"`
 				}
 				err := rows.Scan(&row.Name, &row.Players)
 				if err != nil {
 					logger.Error("Error scanning", zap.String("uri", r.URL.RawPath), zap.Error(err))
 					continue
+				}
+				if image := getPicforGameName(row.Name); image != "" {
+					row.Image = "/api/v0/cdn/" + image
 				}
 				rval = append(rval, row)
 			}
