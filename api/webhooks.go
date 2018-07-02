1438b010bc232f7979ac1357617f571cbb9be05c
apokalyptik <apokalyptik@apokalyptik.com>
2018-07-02 04:25:48 +0000

    just touch a file which signals the need for a rebuild

--- api/webhooks.go
+++ api/webhooks.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"os/exec"
-	"time"
 
 	"github.com/FederationOfFathers/dashboard/bot"
 )
@@ -31,58 +29,10 @@ func init() {
 			}
 			r.Body.Close()
 			Logger.Warn(fmt.Sprintf("%#v", payload))
-			go func() {
-				if payload.Ref == "refs/heads/dev" {
-					bot.SendMessage("#-fof-dashboard", "rebuilding dev ui")
-					now := time.Now()
-					_, err := exec.Command(
-						fmt.Sprintf(
-							"%s/services/dashboard-dev/rebuild-ui",
-							os.Getenv("HOME"),
-						),
-					).CombinedOutput()
-					if err != nil {
-						bot.SendMessage(
-							"#-fof-dashboard",
-							fmt.Sprintf("error rebuilding dev ui: ```%s```", err.Error()),
-						)
-					} else {
-						bot.SendMessage(
-							"#-fof-dashboard",
-							fmt.Sprintf("dev ui rebuilt in %s", time.Now().Sub(now).String()),
-						)
-						time.Sleep(1)
-					}
-					err = exec.Command("/usr/bin/sv", "restart", fmt.Sprintf("%s/services/dashboard-dev/", os.Getenv("HOME"))).Run()
-					if err != nil {
-						Logger.Warn(err.Error())
-					}
-				} else if payload.Ref == "refs/heads/master" {
-					bot.SendMessage("#-fof-dashboard", "rebuilding production ui")
-					now := time.Now()
-					_, err := exec.Command(
-						fmt.Sprintf(
-							"%s/services/dashboard/update",
-							os.Getenv("HOME"),
-						),
-					).CombinedOutput()
-					if err != nil {
-						bot.SendMessage(
-							"#-fof-dashboard",
-							fmt.Sprintf("error rebuilding production ui: ```%s```", err.Error()),
-						)
-					} else {
-						bot.SendMessage(
-							"#-fof-dashboard",
-							fmt.Sprintf("production ui rebuilt in %s", time.Now().Sub(now).String()),
-						)
-					}
-					err = exec.Command("/usr/bin/sv", "restart", fmt.Sprintf("%s/services/dashboard/", os.Getenv("HOME"))).Run()
-					if err != nil {
-						Logger.Warn(err.Error())
-					}
-				}
-			}()
+			fp, _ := os.OpenFile("queue-rebuild", os.O_RDONLY|os.O_CREATE, 0666)
+			if fp != nil {
+				fp.Close()
+			}
 		},
 	)
 }
