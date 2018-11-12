0e745420d79dcc4b576e2cdbcd2e634a27762311
Bear Cherian <bearcherian@gmail.com>
2018-11-12 11:58:48 -0500

    Adding v1/auth/team-tool to use the GamerTag

--- api/team-tool-auth.go
+++ api/team-tool-auth.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/FederationOfFathers/dashboard/bridge"
+	"go.uber.org/zap"
 )
 
 func init() {
@@ -20,6 +21,25 @@ func init() {
 			id := getSlackUserID(r)
 			user, _ := bridge.Data.Slack.User(id)
 			json.NewEncoder(w).Encode(bridge.OldEventToolAuthorization(user.Name))
+		},
+	)
+
+	// V1 - uses member XBL Gamer Tag
+	Router.Path("/api/v1/auth/team-tool").Methods("GET").HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			var err error
+			r, err = authorized(w, r)
+			if err != nil {
+				w.Write([]byte(`""`))
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			id := getSlackUserID(r)
+			member, err := DB.MemberBySlackID(id)
+			if err != nil {
+				Logger.Error("Unable to get member", zap.Error(err), zap.String("slackId", id))
+			}
+			json.NewEncoder(w).Encode(bridge.OldEventToolAuthorization(string(member.Xbl)))
 		},
 	)
 }
