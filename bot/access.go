8cc9fde6f277a3fc0c1d2e5e6a699ed85c44b22f
Bear Cherian <bearcherian@gmail.com>
2019-03-03 00:49:53 -0500

    * Enabling uncategorized channels for events * Adding category ID for filtering/sorting * Adding verified bool to ping * Adding ids of verified and admin roles

--- bot/access.go
+++ bot/access.go
@@ -1,9 +1,11 @@
 package bot
 
 // role IDs
-var adminRoles = []string{""}
+var adminRoles = []string{"439874952112504833", "316736287065243660"}
 
-// IsUserIDAdmin checkes the given Discord ID to check if the user has an admin role
+var verifiedRole = "439875158610542592"
+
+// IsUserIDAdmin checks the given Discord ID to check if the user has an admin role as defined in the bot package
 func IsUserIDAdmin(userID string) (bool, error) {
 	m, e := data.Member(userID)
 	if e != nil {
@@ -15,6 +17,23 @@ func IsUserIDAdmin(userID string) (bool, error) {
 			if ur == role {
 				return true, nil
 			}
+		}
+	}
+
+	return false, nil
+}
+
+// IsUserIDVerified checks if the Discord user with the id given has the verified role as defined in the bot package
+func IsUserIDVerified(userID string) (bool, error) {
+
+	m, e := data.Member(userID)
+	if e != nil {
+		return false, e
+	}
+
+	for _, ur := range m.Roles {
+		if ur == verifiedRole {
+			return true, nil
 		}
 	}
 
