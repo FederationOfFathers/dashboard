3df76c3e295e486fbc7738555923095bd3fe2465
Bear Cherian <bearcherian@gmail.com>
2019-04-12 22:25:11 -0400

    Refactoring temp channel for reusability

--- bot/temp-channel.go
+++ bot/temp-channel.go
@@ -159,73 +159,136 @@ func (d *DiscordAPI) leaveTempChannelHandler(s *discordgo.Session, event *discor
 }
 
 func (d *DiscordAPI) mindTempChannels() {
-	tick := time.Tick(1 * time.Hour) // 1 hour mind, because this could have been started at any time
+	hourTick := time.Tick(1 * time.Hour) // 1 hour mind, because this could have been started at any time
+	d.setChannelAssignMessage()
 	for {
 		select {
-		case <-tick:
-			channels := d.guildChannels()
-			memberChannels := []*Channel{}
+		case <-hourTick:
+			d.purgeOldTempChannels()
+		}
+	}
+}
 
-			// get channels of member channels category
-			for _, category := range channels.Categories {
-				if category.ID == memberCategoryID {
-					memberChannels = category.Channels
-					break
+func (d *DiscordAPI) purgeOldTempChannels() {
+
+	memberChannels := d.textChannelsInCategory(memberCategoryID)
+
+	for _, channel := range memberChannels {
+		// get channel data
+		ch, err := d.discord.Channel(channel.ID)
+		if err != nil {
+			Logger.Error("could not get channel", zap.String("channel_id", channel.ID), zap.Error(err))
+			continue
+		}
+
+		// skip non text channels
+		if ch.Type != discordgo.ChannelTypeGuildText {
+			continue
+		}
+
+		// get last message in channel
+		lastMessage, err := d.discord.ChannelMessage(ch.ID, ch.LastMessageID)
+		if err != nil {
+			// no last message means a new channel
+			continue
+		}
+
+		// get time of last message
+		lastMessageTime, err := lastMessage.Timestamp.Parse()
+		if err != nil {
+			Logger.Error("unable to parse timestamp. check for API changes", zap.String("message", string(lastMessage.Timestamp)), zap.Error(err))
+			continue
+		}
+
+		// if more than 2 days, delete
+		if time.Since(lastMessageTime) > (time.Hour * 48) {
+
+			// find role id by name and delete
+			role, err := d.FindGuildRoleByName(fmt.Sprintf(memberChannelRoleFmt, channel.Name))
+			if err != nil {
+				Logger.Error("unable to find role", zap.Error(err), zap.String("channel", channel.Name ))
+			} else {
+				if err := d.discord.GuildRoleDelete(d.Config.GuildId, role.ID); err != nil {
+					Logger.Error("unable to delete role", zap.Error(err), zap.String("role", role.ID))
 				}
 			}
 
-			for _, channel := range memberChannels {
-				// get channel data
-				ch, err := d.discord.Channel(channel.ID)
-				if err != nil {
-					Logger.Error("could not get channel", zap.String("channel_id", channel.ID), zap.Error(err))
-					continue
-				}
 
-				// skip non text channels
-				if ch.Type != discordgo.ChannelTypeGuildText {
-					continue
-				}
-
-				// get last message in channel
-				lastMessage, err := d.discord.ChannelMessage(ch.ID, ch.LastMessageID)
-				if err != nil {
-					// no last message means a new channel
-					continue
-				}
-
-				// get time of last message
-				lastMessageTime, err := lastMessage.Timestamp.Parse()
-				if err != nil {
-					Logger.Error("unable to parse timestamp. check for API changes", zap.String("message", string(lastMessage.Timestamp)), zap.Error(err))
-					continue
-				}
-
-				// if more than 2 days, delete
-				if time.Since(lastMessageTime) > (time.Hour * 48) {
-
-					// find role id by name and delete
-					role, err := d.FindGuildRoleByName(fmt.Sprintf(memberChannelRoleFmt, channel.Name))
-					if err != nil {
-						Logger.Error("unable to find role", zap.Error(err), zap.String("channel", channel.Name ))
-					} else {
-						if err := d.discord.GuildRoleDelete(d.Config.GuildId, role.ID); err != nil {
-							Logger.Error("unable to delete role", zap.Error(err), zap.String("role", role.ID))
-						}
-					}
-
-
-					// channel delete
-					_, err = d.discord.ChannelDelete(ch.ID)
-					if err != nil {
-						Logger.Error("unable to delete member channel", zap.String("channel", ch.ID), zap.Error(err))
-						continue
-					} else {
-						Logger.Info("deleted channel", zap.String("channel", ch.ID), zap.String("name", ch.Name))
-					}
-
-				}
+			// channel delete
+			_, err = d.discord.ChannelDelete(ch.ID)
+			if err != nil {
+				Logger.Error("unable to delete member channel", zap.String("channel", ch.ID), zap.Error(err))
+				continue
+			} else {
+				Logger.Info("deleted channel", zap.String("channel", ch.ID), zap.String("name", ch.Name))
 			}
+
+		}
+	}
+}
+
+func (d *DiscordAPI) setChannelAssignMessage() {
+	// find channel assign channel
+	memberChannels := d.textChannelsInCategory(memberCategoryID)
+	fmt.Printf("uncut memberChannels: %v\n", memberChannels)
+	var assignChannel *Channel
+
+	for i, ch := range memberChannels {
+		if ch.Name == channelAssignName {
+			assignChannel = ch
+
+			// remove the channel assign channel
+			memberChannels[i] = memberChannels[len(memberChannels)-1]
+			memberChannels = memberChannels[:len(memberChannels)-1]
+			break
+		}
+	}
+
+	fmt.Printf("memberChannels: %v\n", memberChannels)
+
+	if assignChannel == nil || assignChannel.ID == ""{
+		Logger.Warn("unable to locate channel-assign channel")
+		return
+	}
+
+	// remove all channel messages //todo need to redo
+	d.clearChannelMessages(assignChannel.ID)
+
+	// create new messages
+	for _, xch := range memberChannels {
+		fmt.Printf("adding line for %s", xch.Name)
+		msg, err := d.discord.ChannelMessageSend(assignChannel.ID, fmt.Sprintf("<#%s>", xch.ID))
+		if err != nil {
+			Logger.Error("channel assign message failed", zap.String("channel", xch.ID), zap.Error(err))
+			return
+		}
+
+		err = d.discord.MessageReactionAdd(assignChannel.ID, msg.ID, "✅")
+		if err != nil {
+			Logger.Error("unable to add reaction", zap.String("message", msg.ID), zap.Error(err))
+			return
+		}
+
+	}
+
+}
+
+func (d *DiscordAPI) clearChannelMessages(channelID string) {
+
+	messages, err := d.discord.ChannelMessages(channelID, 50, "", "", "")
+	if err != nil {
+		Logger.Error("Unable to access channel messages", zap.String("channelId", channelID), zap.Error(err))
+		return
+	}
+	for _, message := range messages {
+		if message.Author.ID == d.Config.ClientId {
+			err := d.discord.ChannelMessageDelete(channelID, message.ID)
+			if err == nil {
+				Logger.Info("Removed bot message", zap.String("messageId", message.ID))
+			} else {
+				Logger.Error("Unable to remove message", zap.String("messageId", message.ID), zap.Error(err))
+			}
+
 		}
 	}
 }
