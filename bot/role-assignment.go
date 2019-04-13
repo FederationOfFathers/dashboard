c7a920315fddf0825691da1a226fbaca24e9f299
Bear Cherian <bearcherian@gmail.com>
2019-04-13 11:21:46 -0400

    Moving role functions

--- bot/role-assignment.go
+++ bot/role-assignment.go
@@ -168,41 +168,4 @@ func (d *DiscordAPI) createRoleMessages() {
 	}
 }
 
-func (d DiscordAPI) addRoleToUser(userId string, roleId string) {
-	err := d.discord.GuildMemberRoleAdd(d.Config.GuildId, userId, roleId)
-	if err != nil {
-		Logger.Error("could not add role",
-			zap.String("userId", userId),
-			zap.String("roleId", roleId),
-			zap.Error(err))
-	} else {
-		Logger.Info("added role to user",
-			zap.String("userId", userId),
-			zap.String("roleId", roleId),
-		)
-	}
-}
 
-func (d DiscordAPI) removeRoleFromUser(userId string, roleId string) {
-	err := d.discord.GuildMemberRoleRemove(d.Config.GuildId, userId, roleId)
-	if err != nil {
-		Logger.Error("could not remove role", zap.Error(err))
-	} else {
-		Logger.Info("removed role from user",
-			zap.String("userId", userId),
-			zap.String("roleId", roleId),
-		)
-	}
-}
-func (d *DiscordAPI) listRoles() {
-	roles, err := d.discord.GuildRoles(d.Config.GuildId)
-
-	if err != nil {
-		Logger.Error("Could not get roles", zap.Error(err))
-		return
-	}
-
-	for i, role := range roles {
-		Logger.Info(fmt.Sprintf("Role %d", i), zap.Any("role", role))
-	}
-}
