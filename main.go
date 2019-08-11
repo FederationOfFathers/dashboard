f21575bd328e2ec49869fa69dba96fdd21fabace
Bear Cherian <bearcherian@gmail.com>
2019-08-10 22:43:41 -0500

    Sending info level logs to rollbar

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func init() {
 	logger = logger.Named("main")
 
 	// ROLLBAR
-	rollbarCore := rollzap.NewRollbarCore(zapcore.WarnLevel)
+	rollbarCore := rollzap.NewRollbarCore(zapcore.InfoLevel)
 	if err := unmarshalConfig("cfg-rollbar.yml", &rollbarCfg); err != nil {
 		logger.Error("Unable to unmarshal rollbar config", zap.Error(err))
 	} else if rollbarCfg.Token != "" {
